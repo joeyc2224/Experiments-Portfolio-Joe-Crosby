35f531786b51d70c06b20cdcab2c97c67de6f256
Joe Crosby <64789836+joeyc2224@users.noreply.github.com>
2023-04-25 18:36:31 +0100

    Heart rate sensor code

---
+++ MattersOfTheHeart/HeartRateSensor/HeartRateSensor.ino
@@ -0,0 +1,40 @@
+#define USE_ARDUINO_INTERRUPTS true
+#include <PulseSensorPlayground.h>  // Includes the PulseSensor Library.
+
+//  Variables
+const int PulseWire = 0;
+const int LED = LED_BULITIN;
+int Threshold = 550;
+
+PulseSensorPlayground pulseSensor;  // Creates a PulseSensor object called "pulseSensor"
+
+
+void setup() {
+
+  Serial.begin(9600);  // Serial Monitor
+  pulseSensor.analogInput(PulseWire);
+  pulseSensor.blinkOnPulse(LED);
+  pulseSensor.setThreshold(Threshold);
+
+
+  if (pulseSensor.begin()) {  //checks that pulsesensor object is present
+    Serial.println("Created pulseSensor Object");
+  }
+}
+
+
+
+void loop() {
+
+
+
+  if (pulseSensor.sawStartOfBeat()) {  // constantly checks if a beat happens
+    int myBPM = pulseSensor.getBeatsPerMinute();
+
+    Serial.println("♥  A HeartBeat Happened ! ");
+    Serial.print("BPM: ");
+    Serial.println(myBPM);
+  }
+
+  delay(20);
+}
